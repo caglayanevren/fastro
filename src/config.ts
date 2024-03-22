7f920c95ab20d2c84796d18f9d6d474e31574801
Evren Caglayan <caglayanevren@gmail.com>
2024-03-23 01:44:28 +0700

    fix: og image ...

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const FinarExperienceYear = new Date().getFullYear() - new Date("1994").g
 export const lastYear = new Date().getFullYear() - 1;
 export const SITE_TITLE = "FİNAR";
 export const SITE_DESCRIPTION = `Faaliyet raporu alanında ${FinarExperienceYear} yıllık bir deneyime sahibiz. Finar olarak işinin ehli ve geniş kadromuzla ${lastYear} yılında onlarca şirkete bu alanda hizmet verdik. İşlerimizi yakından inceleyin.`;
-export const SEO_IMAGE = "~/images/Finar_Iletisim_Gorsel.png";
+export const SEO_IMAGE = "/og/finar-og.png";
 export const AWARD_IMAGE = "~/images/AWARD_WINNER.png";
 export const DAHA_FAZLA_BILGI = "DAHA FAZLA BİLGİ";
 
