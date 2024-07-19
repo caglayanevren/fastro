4f80a85c95f5ccc78f04cab785fd0064ed95fb59
Evren Çağlayan <caglayanevren@gmail.com>
2024-07-20 00:19:46 +0300

    add: new content

---
+++ src/libs/anasayfa-swipers.ts
@@ -0,0 +1,48 @@
+import Swiper from "swiper";
+import { EffectFade, Autoplay, Pagination } from "swiper/modules";
+import type { SwiperOptions, Swiper as aSwiper } from "swiper/types";
+
+const haberswiperParams: SwiperOptions = {
+	modules: [Pagination, Autoplay],
+	spaceBetween: 0,
+	loop: true,
+	slidesPerView: 1,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		enabled: true,
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	effect: "creative",
+	creativeEffect: {
+		prev: {
+			translate: [0, "-100%", 0],
+		},
+		next: {
+			translate: [0, "100%", 0],
+		},
+	},
+};
+const duyuruswiperParams: SwiperOptions = {
+	modules: [Autoplay, EffectFade],
+	speed: 1000,
+	loop: true,
+	effect: "fade",
+	fadeEffect: {
+		crossFade: true,
+	},
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	//breakpoints: {
+	//	768: {
+	//	},
+	//},
+};
+
+const haberswiper: aSwiper = new Swiper(".haberswiper", haberswiperParams);
+const duyuruswiper: aSwiper = new Swiper(".duyuruswiper", duyuruswiperParams);
