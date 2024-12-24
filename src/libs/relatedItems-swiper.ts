d2a26555ec9d736faf23f9e543535d976698a5c7
Evren Çağlayan <caglayanevren@gmail.com>
2024-12-24 19:47:28 +0300

    add relatedCollectionItems component to some items pages

---
+++ src/libs/relatedItems-swiper.ts
@@ -0,0 +1,29 @@
+import Swiper from "swiper";
+import { Autoplay, Navigation } from "swiper/modules";
+import type { SwiperOptions, Swiper as aSwiper } from "swiper/types";
+
+const swiperParams: SwiperOptions = {
+	modules: [Navigation, Autoplay],
+	slidesPerView: 3,
+	spaceBetween: 30,
+	loop: true,
+	navigation: {
+		enabled: false,
+	},
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		768: {
+			slidesPerView: 4,
+			navigation: {
+				enabled: false,
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		},
+	},
+};
+
+const relatedItemsSwiper: aSwiper = new Swiper(".relatedItemsSwiper", swiperParams);
